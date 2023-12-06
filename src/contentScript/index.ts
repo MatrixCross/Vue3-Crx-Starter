d679167629b9d58661ac1d0fce099b455600d233
wyatex <wyatex@qq.com>
2023-12-06 18:26:00 +0800

    feat: 添加提示

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -11,12 +11,20 @@ import 'uno.css'
   container.id = rootIdName
   const root = document.createElement('div')
   const styleEl = document.createElement('link')
-  const shadowDOM =
-    container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
   styleEl.setAttribute('rel', 'stylesheet')
   styleEl.setAttribute('href', chrome.runtime.getURL('contentScript/style.css'))
+
+  // 使用shadow-dom可以隔离样式
+  const shadowDOM =
+    container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
   shadowDOM.appendChild(styleEl)
   shadowDOM.appendChild(root)
+
+  // 但是naive-ui的样式因为注入在外层，用shadow-dom就会导致没办法用naive-ui的样式
+  // 如果不怕样式影响外层页面，可以直接不用shadow-dom，注释调上面几行，用下面几行
+    // document.body.appendChild(styleEl)
+    // document.body.appendChild(root)
+
   document.body.appendChild(container)
 
   const app = createApp(App)
